9de74aa2d4ace0bb7898d1f6c0abc7e88bcfde4c
dominhit215@gmail.com <dominhit215@gmail.com>
2024-10-29 13:18:02 +0700

    configure Routing using React-Router-Dom

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -33,6 +33,11 @@ export default function Home() {
                                     <td>{users.name}</td>
                                     <td>{users.username}</td>
                                     <td>{users.email}</td>
+                                    <td>
+                                        <button className='btn btn-primary mx-2'>View</button>
+                                        <button className='btn btn-outline-primary mx-2'>Edit</button>
+                                        <button className='btn btn-danger mx-2'>Delete</button>
+                                    </td>
                                 </tr>
                             ))
                         }
