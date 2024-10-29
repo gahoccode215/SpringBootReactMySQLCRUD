620b7ffdac5c8428e39a787bb638708460e4d556
dominhit215@gmail.com <dominhit215@gmail.com>
2024-10-29 13:42:16 +0700

    edit user using putmapping

--- backend/src/main/java/com/gahoccode215/backend/controller/UserController.java
+++ backend/src/main/java/com/gahoccode215/backend/controller/UserController.java
@@ -27,4 +27,14 @@ public class UserController {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
+    @PutMapping("/users/{id}")
+    public User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
 }
