aae8b9325abe4daab5987448787af61badb2f97c
dominhit215@gmail.com <dominhit215@gmail.com>
2024-10-29 13:51:40 +0700

    edit user using axios put

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 export default function Home() {
 
     const [users, setUsers] = useState([]);
@@ -35,7 +36,7 @@ export default function Home() {
                                     <td>{users.email}</td>
                                     <td>
                                         <button className='btn btn-primary mx-2'>View</button>
-                                        <button className='btn btn-outline-primary mx-2'>Edit</button>
+                                        <Link to={`/edituser/${users.id}`} className='btn btn-outline-primary mx-2'>Edit</Link>
                                         <button className='btn btn-danger mx-2'>Delete</button>
                                     </td>
                                 </tr>
