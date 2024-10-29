fa4a8a44fe36ce6ceba9fb83462dc3b739049587
dominhit215@gmail.com <dominhit215@gmail.com>
2024-10-29 12:27:09 +0700

    get all user

--- backend/src/main/java/com/gahoccode215/backend/controller/UserController.java
+++ backend/src/main/java/com/gahoccode215/backend/controller/UserController.java
@@ -3,9 +3,12 @@ package com.gahoccode215.backend.controller;
 import com.gahoccode215.backend.model.User;
 import com.gahoccode215.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,5 +19,8 @@ public class UserController {
     public User newUser(@RequestBody User user) {
         return userRepository.save(user);
     }
-
+    @GetMapping("/users")
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
 }
